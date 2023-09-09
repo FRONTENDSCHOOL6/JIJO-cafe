c172e875771799e786bd280d44e1e6a4e421bb47
youngniiiiii <hanuje_@naver.com>
2023-09-10 01:59:47 +0900

    🦁 modify : 검색창, 리스트 페이지에 상태전달

--- src/views/Notice.jsx
+++ src/views/Notice.jsx
@@ -1,106 +1,38 @@
-import Button from '@/components/Button'
 import { Helmet } from 'react-helmet-async'
-import Input from '@/components/Input'
-import { useId } from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import pb from '@/api/pocketbase'
+
 import MenuTitle from '@/components/MenuTitle'
-import yyyymmddDate from '@/utils/yyyymmddDate'
+
+import NoticeList from '@/components/Notice/NoticeList'
+import { useState } from 'react'
+import NoticeSearchFilter from '@/components/Notice/NoticeSearchFilter'
 
 function Notice() {
-  const id = useId()
-  const [data, setData] = useState(null)
-  const [status, setStatus] = useState('pending')
-  // const [views, setViews] = useState(0);
+  const [searchOption, setSearchOption] = useState('noticeTitle') //select 태그
+  const [searchText, setSearchText] = useState('') //input 창
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setStatus('loading')
-      try {
-        const noticeItems = await pb.collection('notices').getFullList()
-        console.log(noticeItems)
-        setData(noticeItems)
-        console.log(data)
-        setStatus('success')
-      } catch (error) {
-        setStatus('error')
-      }
-    }
-    fetchData()
-  }, [])
+  // const [views, setViews] = useState(0);
+  // error 상태 사용?
+
+  const handleViewClick = () => {
+    // 조회수+1 증가
+    setViews(views + 1)
+  }
 
   return (
     <>
+      <Helmet>
+        <title>지조소식 - 공지사항</title>
+      </Helmet>
       <MenuTitle title="JIJO NEWS"> JIJO NOTICE</MenuTitle>
 
       <section className="max-w-screen-xl mx-auto px-5 py-jj_60">
-        <Helmet>
-          <title>지조소식 - 공지사항</title>
-        </Helmet>
         <div className="text-center ">
           <h2 className="text-jj_43 font-bold ">카페 지조 공지사항</h2>
           <p className="m-10"> 카페 지조 소식을 알려드립니다.</p>
         </div>
-        <form>
-          <label htmlFor={id} className="sr-only">
-            검색창
-          </label>
-          <select id={id} name="notice" className="border px-jj_15 mr-[0.3125rem] rounded-sm h-[2.8125rem]">
-            <option value="noticeTitle">제목</option>
-            <option value="noticeDescription">내용</option>
-            <option value="noticeWriter">작성자</option>
-          </select>
-          <Input placeholder="검색어를 입력하세요" className="bg-white mr-[0.3125rem] border px-jj_15 w-fit"></Input>
-          <Button color="primary" className="px-5 py-[0.625rem] mr-[0.3125rem] mobile:mt-[0.9375rem] mobile:w-full">
-            검색
-          </Button>
-          <Button color="primary" className="px-5  mobile:w-full mobile:my-[0.9375rem]">
-            등록
-          </Button>
-        </form>
-
-        <table className="min-w-max w-full table-auto bg-white my-6 border-t">
-          <thead>
-            <tr className="text-jj_15 leading-normal">
-              <th className="mobile:hidden py-3 px-6 items-center">번호</th>
-              <th className="py-3 px-6 ">제목</th>
-              <th className="mobile:hidden py-3 px-6 ">글쓴이</th>
-              <th className="py-3 px-6 text-center">날짜</th>
-              <th className="mobile:hidden py-3 px-6 ">조회</th>
-            </tr>
-          </thead>
-          <tbody className="text-gray-600 text-sm font-light border-t">
-            {data &&
-              data.map((item, index) => {
-                return (
-                  <tr key={item.id} className="border-b border-gray-200 hover:bg-gray-100">
-                    <td className="py-3 px-6 whitespace-nowrap">
-                      <div className="text-center">
-                        <span className="mobile:hidden font-medium">{data.length - index}</span>
-                      </div>
-                    </td>
-                    <td className="py-3 px-6 text-left">
-                      <div>
-                        <span>{item.noticeTitle}</span>
-                      </div>
-                    </td>
-                    <td className="py-3 px-6 ">
-                      <div className=" mobile:hidden text-center">
-                        <span>{item.noticeWriter}</span>
-                      </div>
-                    </td>
-                    <td className="py-3 px-6 text-center">
-                      <span>{yyyymmddDate(item.noticeDate)}</span>
-                    </td>
-                    <td className=" mobile:hidden py-3 px-6 text-center">
-                      <span>{item.noticeViews}</span>
-                    </td>
-                  </tr>
-                )
-              })}
-          </tbody>
-        </table>
+        <NoticeSearchFilter option={searchOption} onChangeOption={setSearchOption} text={searchText} onChangeText={setSearchText}></NoticeSearchFilter>
+        {/* 상태를 props로 NoticeSearchFilter에 전달 */}
+        <NoticeList option={searchOption} text={searchText}></NoticeList>
       </section>
     </>
   )
