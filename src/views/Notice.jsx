f3b3a32c648cf8b5571fee568e9a596ccfbedebd
youngniiiiii <hanuje_@naver.com>
2023-09-11 23:33:06 +0900

    🦁 modify : pageMainTitle,스피너 컴포넌트 재사용

--- src/views/Notice.jsx
+++ src/views/Notice.jsx
@@ -1,17 +1,19 @@
-import { Helmet } from 'react-helmet-async'
-import { useEffect } from 'react'
 import pb from '@/api/pocketbase'
+import { Helmet } from 'react-helmet-async'
+import { useState, useEffect } from 'react'
 import MenuTitle from '@/components/MenuTitle'
 import NoticeList from '@/components/Notice/NoticeList'
-import { useState } from 'react'
+import JijoSpinner from '@/components/JijoSpinner'
+import PageMainTitle from '@/components/PageMainTitle'
 import NoticeSearchFilter from '@/components/Notice/NoticeSearchFilter'
 
 function Notice() {
+  const [data, setData] = useState(null)
+  const [status, setStatus] = useState('pending')
   const [searchOption, setSearchOption] = useState('noticeTitle') //select 태그
   const [searchText, setSearchText] = useState('') //input 창
   const [reload, setReload] = useState(true) //검색버튼 클릭시 pb 다시 작동하도록
-  const [data, setData] = useState(null)
-  const [status, setStatus] = useState('pending')
+  // error 상태 사용?
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,23 +21,21 @@ function Notice() {
       try {
         let noticeItems
         if (!searchText) {
-          noticeItems = await pb.collection('notices').getFullList()
+          const response = await pb.collection('notices').getList(1, 10)
+          noticeItems = response.items
           console.log(noticeItems)
         } else {
           const response = await pb.collection('notices').getList(1, 10, {
             filter: `(${searchOption} ~ '${searchText}')`,
-            // 필터 수정중
           })
           noticeItems = response.items
           console.log(noticeItems)
         }
-
         setData(noticeItems)
-        // console.log(noticeItems)
-        // console.log(data)
         setStatus('success')
       } catch (error) {
         setStatus('error')
+        console.error('error')
       }
     }
     if (reload) {
@@ -44,34 +44,27 @@ function Notice() {
     }
   }, [reload])
 
-  // const [views, setViews] = useState(0);
-  // error 상태 사용?
+  if (status === 'loading') {
+    return <JijoSpinner />
+  }
+  // 스피너를 로딩 중일 때만 렌더링
 
   const handleReload = () => {
     setReload(true)
   }
-
-  // const handleViewClick = () => {
-  //   // 조회수+1 증가
-  //   setViews(views + 1)
-  // }
 
   return (
     <>
       <Helmet>
         <title>지조소식 - 공지사항</title>
       </Helmet>
-
       <MenuTitle title="JIJO NEWS"> JIJO NOTICE</MenuTitle>
-
       <section className="max-w-screen-xl mx-auto px-5 py-jj_60">
-        <div className="text-center ">
-          <h2 className="text-jj_43 font-bold ">카페 지조 공지사항</h2>
-          <p className="m-10"> 카페 지조 소식을 알려드립니다.</p>
-        </div>
+        <PageMainTitle pageTitleText="카페 지조 공지사항" pageSubTitleText="카페 지조 소식을 알려드립니다."></PageMainTitle>
+
         <NoticeSearchFilter handleReload={handleReload} option={searchOption} onChangeOption={setSearchOption} text={searchText} onChangeText={setSearchText}></NoticeSearchFilter>
         {/* 상태를 props로 NoticeSearchFilter에 전달 */}
-        <NoticeList data={data}></NoticeList>
+        <NoticeList data={data} />
       </section>
     </>
   )
