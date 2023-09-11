357bb12b5b71b8fa33d85c52c38cb00a961b928c
youngniiiiii <hanuje_@naver.com>
2023-09-11 23:05:16 +0900

    🦁 modify : pageMainTitle padding위치 변경

--- src/components/PageMainTitle.jsx
+++ src/components/PageMainTitle.jsx
@@ -1,14 +1,12 @@
-function PageMainTitle({pageTitleText, pageSubTitleText}) {
+function PageMainTitle({ pageTitleText, pageSubTitleText }) {
   return (
     <>
-      <div className="text-center">
-        <h2 className="font-bold text-[2.25rem] pb-3">{pageTitleText}</h2>
-        <span className="pb-[2.625rem] font-light text-[#1c1c1b]">
-          {pageSubTitleText}
-        </span>
+      <div className="text-center pb-[2.625rem] ">
+        <h2 className="font-bold text-jj_43 pb-3">{pageTitleText}</h2>
+        <span className="font-light text-[#1c1c1b]">{pageSubTitleText}</span>
       </div>
     </>
-  );
+  )
 }
 
-export default PageMainTitle;
+export default PageMainTitle
