3fa0cba99a311466cd2858962ea6500fd8442560
rerro <101159509+rorre0@users.noreply.github.com>
2023-09-14 16:30:16 +0900

    🦁 modify : Main-Menu 섹션 마크업 수정 및 Title 영역 컴포넌트로 분리
    [메인] Menu 섹션 마크업 및 스타일링 #95

--- src/views/Main/MenuCarousel.jsx
+++ src/views/Main/MenuCarousel.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation } from "swiper/modules";
 import pb from "@/api/pocketbase";
@@ -6,19 +6,35 @@ import { getPbImageURL } from "@/utils/getPbImageURL";
 import { usePocketBaseFilteredData } from "@/hooks/usePocektBaseData";
 import "swiper/css";
 import "@/styles/Carousel.css";
+import { PrevButton, NextButton } from "@/styles/NevigationArrowButton";
 
 export default function MenuCarousel() {
   pb.autoCancellation(false);
   const { data } = usePocketBaseFilteredData("beverage", 1, 5, 'created >= "2023-09-08 00:00:00"', "beverage");
 
-  const [swiperRef, setSwiperRef] = useState(null);
+  const sliderRef = useRef(null);
+  // const navigationPrevRef = useRef(null);
+  // const navigationNextRef = useRef(null);
+
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slidePrev();
+  }, []);
+
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slideNext();
+  }, []);
 
   return (
     <>
       <Swiper
+        // ref={sliderRef}
         modules={[Navigation]}
-        navigation
-        onSwiper={setSwiperRef}
+        // navigation={{
+        //   prevEl: navigationPrevRef.current,
+        //   nextEl: navigationNextRef.current,
+        // }}
         grabCursor={true}
         spaceBetween={10}
         slidesPerView={2}
@@ -26,22 +42,33 @@ export default function MenuCarousel() {
         loop={true}
         keyboard={true}
         className="menuSwiper"
+        // onSwiper={(swiper) => {
+        //   setTimeout(() => {
+        //     swiper.params.navigation.prevEl = navigationPrevRef.current;
+        //     swiper.params.navigation.nextEl = navigationNextRef.current;
+        //   });
+        // }}
       >
-        {data.items?.map((item, el) => {
-          return (
-            <SwiperSlide key={item.id}>
-              <figure>
-                {<img key={item.id} src={getPbImageURL(item, "image")} alt="" />}
-                <div className="titleSection">
-                  <h5 key={item.id} className="text-jj_18">
-                    {item.title}
-                  </h5>
-                  <figcaption key={item.id}>{item.description}</figcaption>
-                </div>
-              </figure>
-            </SwiperSlide>
-          );
-        })}
+        {data &&
+          data.items?.map((item, el) => {
+            return (
+              <SwiperSlide key={item.id}>
+                <figure>
+                  {<img src={getPbImageURL(item, "image")} alt="" />}
+                  <div className="titleSection">
+                    <h5 className="text-jj_18">{item.title}</h5>
+                    <figcaption>{item.description}</figcaption>
+                  </div>
+                </figure>
+              </SwiperSlide>
+            );
+          })}
+        <div className="absolute top-[10%] right-[330px] borderborder-red-500">
+          {/* <button ref={navigationPrevRef} onClick={handlePrev} className="buttonCss" />
+          <button ref={navigationNextRef} onClick={handleNext} className="buttonCss" /> */}
+          {/* <PrevButton />
+          <NextButton /> */}
+        </div>
       </Swiper>
     </>
   );
