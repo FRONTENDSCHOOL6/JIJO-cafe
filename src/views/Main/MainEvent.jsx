bd8f744e4c448e3e5c906162637161227bda4a72
rerro <101159509+rorre0@users.noreply.github.com>
2023-09-14 16:33:58 +0900

    ✅ add : 메인-쇼핑몰,스토어,이벤트 페이지 추가 및 기본 스타일링
    [메인] ShopingMall 섹션 마크업 및 스타일링 #96
    [메인] Store(매장 찾기) 섹션 마크업 및 스타일링 #99
    [메인] Event 섹션 마크업 및 스타일링 #100

---
+++ src/views/Main/MainEvent.jsx
@@ -0,0 +1,16 @@
+import Button from "../../components/Button";
+import MainTitle from "./MainTitle";
+
+function MainEvent() {
+  return (
+    <section className="flex h-screen py-[10%] bg-primary">
+      <h2 className="sr-only">메인페이지 쇼핑몰</h2>
+      <div className="flex justify-center flex-1  mr-[20px] ml-[5rem]"></div>
+      <div className="flex-1 px-20 py-21">
+        <div className="w-full h-full bg-black"></div>
+      </div>
+    </section>
+  );
+}
+
+export default MainEvent;
