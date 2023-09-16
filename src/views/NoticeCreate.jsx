bf4021350dcf65e0f9020ddb7403bfde73f02cb6
youngniiiiii <hanuje_@naver.com>
2023-09-17 01:30:29 +0900

    ♻️ refactor : [공지등록] form ->컴포넌트 분리

--- src/views/NoticeCreate.jsx
+++ src/views/NoticeCreate.jsx
@@ -1,20 +1,14 @@
 import pb from "@/api/pocketbase"
-// import React, { useRef } from "react"
 import JijoSpinner from "@/components/JijoSpinner"
 import MenuTitle from "@/components/MenuTitle"
 import PageMainTitle from "@/components/PageMainTitle"
 import { Helmet } from "react-helmet-async"
-import Input from "@/components/Input"
-import { useId } from "react"
-import Button from "@/components/Button"
 import { useState } from "react"
-
 import { useNavigate } from "react-router-dom"
-import { useEffect } from "react"
+import DataForm from "@/components/Notice/DataForm"
+import Button from "@/components/Button"
 
 function NoticeCreate() {
-  // const { noticeId } = useParams()
-  const id = useId()
   const Navigate = useNavigate()
   const [fileName, setFileName] = useState("파일이름")
 
@@ -52,39 +46,10 @@ function NoticeCreate() {
       <MenuTitle title="JIJO NEWS"> JIJO NOTICE</MenuTitle>
       <section className="max-w-screen-xl mx-auto px-5 py-jj_60">
         <PageMainTitle pageTitleText="공지사항 등록" pageSubTitleText="카페 지조 관리자 페이지 입니다."></PageMainTitle>
-        <form onSubmit={handleCreate} className="border flex gap-5 flex-col w-[53.75rem] mx-auto px-[5.625rem] py-[3.125rem]">
-          <div className="flex justify-between items-center mt-[3.75rem] ">
-            <Input label="관리자" value="카페 지조" readOnly required name="noticeWriter" placeholder="카페 지조" labelClassName="w-[7.8125rem] " className="bg-white block mr-[0.3125rem] border px-jj_15 w-full"></Input>
-          </div>
-          <div className="flex justify-between items-center">
-            <Input label="제목" required name="noticeTitle" placeholder="제목을 입력하세요" labelClassName="w-[7.8125rem]" className="bg-white mr-[0.3125rem] border px-jj_15 w-full"></Input>
-          </div>
-          <div className="flex justify-between items-center">
-            <label htmlFor={id} className="w-[7.8125rem]">
-              내용
-            </label>
-            <textarea id={id} required name="noticeDescription" className="border rounded w-full" rows="5" cols="33"></textarea>
-            {/* <input type="date" readOnly value={today} hidden /> */}
-          </div>
-          <div>
-            <div className="flex items-center">
-              <span className="w-[6.5rem]">파일첨부</span>
-              <div>
-                <div>
-                  <input className="hidden" name="noticeImage" type="file" id="fileInput" onChange={handleFileChange} />
-                  <input className="h-[2.8125rem] px-4 border border-gray-300  text-gray-500" value={fileName} readOnly />
-                  <label htmlFor="fileInput" className="cursor-pointer font-medium ml-1 py-[0.8rem] px-10  rounded-sm bg-primary">
-                    업로드
-                  </label>
-                </div>
-                <p>jpg, jpeg, png파일만 첨부가능합니다</p>
-              </div>
-            </div>
-          </div>
-          <Button type="submit" color="primary" className="mt-[3.75rem]">
-            등록하기
-          </Button>
-        </form>
+        <DataForm handleCreate={handleCreate} handleFileChange={handleFileChange} fileName={fileName}></DataForm>
+        <Button type="submit" color="primary" className="mt-[3.75rem] mx-auto">
+          등록하기
+        </Button>
       </section>
     </>
   )
