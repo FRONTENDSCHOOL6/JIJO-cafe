3adc8a578609e0ed22612254186dac759d5c5230
rerro <101159509+rorre0@users.noreply.github.com>
2023-09-18 13:44:32 +0900

    ✨ feat : viewPoint에 따라 다른 사이즈 이미지 렌더링
    [메인] Main 섹션 반응형 작업 #106

--- src/views/Main/MainBanner.jsx
+++ src/views/Main/MainBanner.jsx
@@ -1,14 +1,28 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Autoplay } from "swiper/modules";
+import { Pagination, Autoplay, Keyboard } from "swiper/modules";
 import "swiper/css/pagination";
 import "@/styles/Carousel.css";
 import pb from "@/api/pocketbase";
 import { getPbImageURL } from "@/utils/getPbImageURL";
 import { usePocektBaseDataList } from "@/hooks/usePocektBaseData";
+import { useState, useEffect } from "react";
 
 export default function MainBanner() {
   pb.autoCancellation(false);
   const { data } = usePocektBaseDataList("mainBanner");
+  const [mediaQuery, setMediaQuery] = useState(window.innerWidth < 760);
+
+  const screenChange = (e) => {
+    const matches = e.matches;
+    setMediaQuery(matches);
+  };
+
+  useEffect(() => {
+    let matchMedia = window.matchMedia("screen and (max-width:760px)");
+    matchMedia.addEventListener("change", screenChange);
+    return () => matchMedia.removeEventListener("change", screenChange);
+  }, []);
+  console.log(data);
 
   return (
     <Swiper
@@ -24,16 +38,25 @@ export default function MainBanner() {
       }}
       keyboard={{ enabled: true }}
       grabCursor={true}
-      className="mainSwiper"
+      id="mainSwiper"
     >
-      {data &&
-        data?.map((item) => {
-          return (
-            <SwiperSlide key={item.id}>
-              {<img src={getPbImageURL(item, "pcTabletImage")} alt={item.title} className="block object-cover w-full h-screen" />}
-            </SwiperSlide>
-          );
-        })}
+      {mediaQuery
+        ? data &&
+          data?.map((item) => {
+            return (
+              <SwiperSlide key={item.id}>
+                <img src={getPbImageURL(item, "mobileImage")} alt={item.title} className="block object-fill w-full h-screen" />
+              </SwiperSlide>
+            );
+          })
+        : data &&
+          data?.map((item) => {
+            return (
+              <SwiperSlide key={item.id}>
+                <img src={getPbImageURL(item, "pcTabletImage")} alt={item.title} className="block object-fill w-full h-screen" />
+              </SwiperSlide>
+            );
+          })}
     </Swiper>
   );
 }
