c64ac814a4d61cfce39805d27a658133b1259f46
youngniiiiii <hanuje_@naver.com>
2023-09-19 22:49:32 +0900

    ♻️ refactor 공지생성,삭제 리액트뮤테이션 리팩토링

--- src/views/NoticeDetail.jsx
+++ src/views/NoticeDetail.jsx
@@ -2,59 +2,79 @@ import pb from "@/api/pocketbase"
 import JijoSpinner from "@/components/JijoSpinner"
 import MenuTitle from "@/components/MenuTitle"
 import Detail from "@/components/Notice/Detail"
-import { useState } from "react"
-import { useEffect } from "react"
 import { Helmet } from "react-helmet-async"
 import { useNavigate } from "react-router-dom"
 import { useParams } from "react-router-dom"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+
+async function fetchNoticesDetail(noticeId) {
+  const response = await pb.collection("notices").getOne(noticeId)
+  // console.log(response)
+  return response
+}
 
 function NoticeDetail() {
   const { noticeId } = useParams()
-  const [data, setData] = useState(null)
-  const [status, setStatus] = useState("pending")
-  const [error, setError] = useState(null)
   const Navigate = useNavigate()
+  const queryClient = useQueryClient()
+  const noticeDelete = useMutation({
+    mutationFn: (id) =>
+      pb
+        .collection("notices")
+        .delete(id)
+        .then((response) => response.data),
+    onSuccess: (savedTodo) => {
+      queryClient.invalidateQueries({
+        queryKey: ["notice"],
+      })
+      Navigate("/bbs/notice") // 삭제 후 공지사항 목록 페이지로 이동
+    },
+  })
 
   pb.autoCancellation(false) // 오토캔슬 false
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setStatus("loading")
-      try {
-        const noticeItems = await pb.collection("notices").getOne(noticeId) //단일 데이터 가져올때 getOne
-        setData(noticeItems)
-        setStatus("success")
-      } catch (error) {
-        setStatus("error")
-        setError(error)
-      }
-    }
-    fetchData()
-  }, [noticeId])
+  const { isLoading, data, isError, error } = useQuery({
+    //리액트 쿼리 사용
+    queryKey: ["noticeDetail", noticeId],
+    queryFn: () => fetchNoticesDetail(noticeId),
+    staleTime: 1 * 1000 * 60 * 60 * 24 * 7,
+  })
 
-  if (status === "loading") {
-    return <JijoSpinner />
-  }
-  // 데이터 가져오는 중(로딩)일 때 표시할 화면
-
-  if (status === "error") {
+  if (isLoading) {
     return (
-      <div role="alert" className="flex flex-col h-[calc(100vh_-_70px)] w-auto justify-center items-center ">
-        <p>{error.toString()}</p>
-        <p>알 수 없는 오류가 발생했습니다.</p>
+      <div>
+        <JijoSpinner />
       </div>
     )
   }
 
-  const handleDelete = async () => {
+  if (isError) {
+    return <div role="alert">{error.toString()}</div>
+  }
+
+  const handleDelete = async (e) => {
+    e.preventDefault()
+    // noticeDelete.mutate(noticeId)
+    // Navigate("/bbs/notice")
+
     try {
       // 공지사항 삭제 함수
-      await pb.collection("notices").delete(noticeId) // PocketBase를 사용하여 공지사항을 삭제
+      noticeDelete.mutate(noticeId)
+      // await pb.collection("notices").delete(noticeId) // PocketBase를 사용하여 공지사항을 삭제
+
+      if (noticeDelete.isLoading) {
+        return (
+          <div>
+            <JijoSpinner />
+          </div>
+        )
+      }
+
       // 삭제가 성공적으로 이루어졌을 때 실행할 코드
       console.log("게시글이 삭제되었습니다.") // 삭제 완료 메시지 출력 또는 다른 작업 수행 가능
 
       // 삭제 후 특정 페이지로 리다이렉트
-      Navigate("/bbs/notice") // 삭제 후 공지사항 목록 페이지로 이동
+      // Navigate("/bbs/notice") // 삭제 후 공지사항 목록 페이지로 이동
     } catch (error) {
       // 삭제 중에 오류가 발생했을 때 실행
       console.error("게시글 삭제 중 오류 발생:", error)
