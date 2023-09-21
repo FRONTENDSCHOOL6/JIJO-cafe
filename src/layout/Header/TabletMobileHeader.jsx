ef0d4ea6c6024ca821f28c04bf2ed03259ea3115
Suwan <imissyou5466@gmail.com>
2023-09-21 09:40:08 +0900

    ✨  반응형 헤더 링크 이동 시 탭매뉴 닫히는 기능 구현 / 로그인 오류 수정

--- src/layout/Header/TabletMobileHeader.jsx
+++ src/layout/Header/TabletMobileHeader.jsx
@@ -8,25 +8,28 @@ import useAuthStore from "@/store/store";
 import SignInModal from "@/components/SignInModal";
 import toast from "react-hot-toast";
 import {kakaoLogout} from "@/utils/kakaoLogout";
+import {useState} from "react";
+import {useLocation} from "react-router-dom";
+import {useEffect} from "react";
 
-function TabletMobileHeader({
-  siginInView,
-  siginViewHandler,
-  setIsClickedSignin,
-}) {
+function TabletMobileHeader() {
   /* 마우스 클릭에 따른 햄버거 탭과 닫기 탭 렌더링 여부를 관리하는 상태 */
   const [isToggleTabButton, setIsToggleTabButton] = useToggle(false);
 
   /* 링크이동 시 해당 탭메뉴가 닫히는 기능 */
+  const location = useLocation();
+  useEffect(() => {
+    setIsToggleTabButton(false);
+  }, [location]);
 
   /* 인증 정보에 따른 로그인 ➡️ 로그아웃으로 변경 */
   const isAuth = useAuthStore((state) => state.isAuth);
 
-  // /* 클릭시 로그인모달 렌더링 */
-  // const [isClickedSignin, setIsClickedSignin] = useState(false);
-  // const handleClickSignin = () => {
-  //   setIsClickedSignin(!isClickedSignin);
-  // };
+  /* 클릭시 로그인모달 렌더링 */
+  const [isClickedSignin, setIsClickedSignin] = useState(false);
+  const handleClickSignin = () => {
+    setIsClickedSignin(!isClickedSignin);
+  };
 
   /* 일반사용자 로그아웃 및 카카오 사용자 로그아웃 */
   const signOut = useAuthStore((state) => state.signOut);
@@ -102,7 +105,7 @@ function TabletMobileHeader({
                   </div>
                 ) : (
                   <div className={S.titleLink}>
-                    <li onClick={siginViewHandler} className="cursor-pointer">
+                    <li onClick={handleClickSignin} className="cursor-pointer">
                       로그인
                     </li>
                   </div>
@@ -113,11 +116,11 @@ function TabletMobileHeader({
                       로그아웃
                     </li>
                   ) : (
-                    <li onClick={siginViewHandler} className="cursor-pointer">
+                    <li onClick={handleClickSignin} className="cursor-pointer">
                       로그인
                     </li>
                   )}
-                  {siginInView && (
+                  {isClickedSignin && (
                     <SignInModal setIsClickedSignin={setIsClickedSignin} />
                   )}
                 </div>
