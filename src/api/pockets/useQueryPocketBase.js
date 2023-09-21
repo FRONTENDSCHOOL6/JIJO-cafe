2bb80419afe22f22af1f4d38a5fe23a3f8967a3d
youngniiiiii <hanuje_@naver.com>
2023-09-22 02:17:55 +0900

    ✨ feat : [페이지네이션] getPagination 추가

--- src/api/pockets/useQueryPocketBase.js
+++ src/api/pockets/useQueryPocketBase.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query"
-import { getProducts, getBeverage, getFoods, getNotices } from "@/api/pockets"
+import { getProducts, getBeverage, getFoods, getNotices, getEvents, getPagination } from "@/api/pockets"
 
 // PocketBase 데이터베이스의 데이터를 요청한 후 캐싱하는 커스텀 훅 함수
 // 예시에서는 getProducts를 사용 (추후 queryFn에 연결될 함수를 외부로 분리 가능)
@@ -49,5 +49,15 @@ export const useQueryGetEvents = (key, page, perPage, options) => {
     queryFn: () => getEvents(page, perPage, options),
     keepPreviousData: true,
     ...options,
+  })
+}
+
+export const useQueryPocketBase = (key, page, perPage, dependency, options) => {
+  return useQuery({
+    queryKey: [key, page, dependency],
+    queryFn: () => getPagination(key, page, perPage, options),
+    keepPreviousData: true,
+    // ...options,
+    staleTime: 1000 * 60 * 5,
   })
 }
