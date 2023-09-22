5010528c6e31770ba775f1a835f930b9230abab3
youngniiiiii <hanuje_@naver.com>
2023-09-22 11:14:51 +0900

    🦁 modify : drink 업데이트

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,13 @@
-import {getPbImageURL} from "@/utils/getPbImageURL";
-import {numberWithComma} from "@/utils/numberWithComma";
-import {useState} from "react";
-import useCartStore from "@/store/cartStore";
-import ProductModal from "./ProductModal";
-import LazyImage from "@/utils/LazyImage";
-import toast from "react-hot-toast";
+import cart from "@/assets/images/menu/cart.svg"
+import { getPbImageURL } from "@/utils/getPbImageURL"
+import { numberWithComma } from "@/utils/numberWithComma"
+import { useState } from "react"
+import useCartStore from "@/store/cartStore"
+import ProductDialog from "./ProductDialog"
+import LazyImage from "@/utils/LazyImage"
+import toast from "react-hot-toast"
 
-function Products({data}) {
+function Products({ data }) {
   return (
     <div className="itemWrap">
       <ul className="grid gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mobile:grid-cols-2">
@@ -15,58 +16,47 @@ function Products({data}) {
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
-export default Products;
+export default Products
 
-function ProductItem({item, ...restProps}) {
-  const [isClicked, setIsClicked] = useState(false);
+function ProductItem({ item, ...restProps }) {
+  const [isClicked, setIsClicked] = useState(false)
   const handleClick = () => {
-    setIsClicked(!isClicked);
-  };
+    setIsClicked(!isClicked)
+  }
 
   /* Cart 배열에 새로운 아이템 */
-  const add = useCartStore((state) => state.add);
+  const add = useCartStore((state) => state.add)
   const handleAddToCart = () => {
-    add(item);
+    add(item)
     toast.success(`장바구니에 ${item.title}가 담겼습니다!`, {
       icon: "🛒",
-    });
-  };
+    })
+  }
 
   return (
     <li key={item.id} className="relative cursor-pointer" {...restProps}>
       <div>
-        <div
-          className="imgFrame relative w-80 h-80 overflow-hidden"
-          onClick={handleClick}>
-          <LazyImage
-            src={getPbImageURL(item, "image")}
-            className="w-full transition-all ease-in hover:scale-110"
-            alt={item.title}
-          />
-          {isClicked && <ProductModal key={item.id} item={item} />}
+        <div className="imgFrame relative w-80 h-80 overflow-hidden" onClick={handleClick}>
+          <LazyImage src={getPbImageURL(item, "image")} className="w-full transition-all ease-in hover:scale-110" alt={item.title} />
+          {isClicked && <ProductDialog key={item.id} item={item} />}
         </div>
         <div className="text py-6">
-          <p className="title text-jj_22 pb-5 mb-[.3125rem] border-b overflow-hidden text-ellipsis whitespace-nowrap">
-            {item.title}
-          </p>
-          <span className="price text-[#1c1c1b] opacity-70 text-jj_14 leading-none">
-            {numberWithComma(item.price)}
-          </span>
-          <p className="desc text-[#1c1c1b] opacity-70 text-jj_14 mobile:text-sm mt-5 overflow-hidden text-ellipsis line-clamp-2">
-            {item.description}
-          </p>
+          <p className="title text-jj_22 pb-5 mb-[.3125rem] border-b overflow-hidden text-ellipsis whitespace-nowrap">{item.title}</p>
+          <span className="price text-[#1c1c1b] opacity-70 text-jj_14 leading-none">{numberWithComma(item.price)}</span>
+          <p className="desc text-[#1c1c1b] opacity-70 text-jj_14 mobile:text-sm mt-5 overflow-hidden text-ellipsis line-clamp-2">{item.description}</p>
         </div>
       </div>
       <button
         onClick={() => {
-          handleAddToCart(item);
+          handleAddToCart(item)
         }}
-        className="absolute bottom-[11.25rem] right-0">
-        <img src="/src/assets/images/menu/cart.svg" alt="" />
+        className="absolute bottom-[11.25rem] right-0"
+      >
+        <img src={cart} alt="" />
       </button>
     </li>
-  );
+  )
 }
--- src/views/Drink.jsx
+++ src/views/Drink.jsx
@@ -1,19 +1,20 @@
-import Categories from "@/components/Categories";
-import JijoSpinner from "@/components/JijoSpinner";
-import MenuBubble from "@/components/MenuBubble";
-import MenuTitle from "@/components/MenuTitle";
-import Products from "@/components/Products";
-import { usePocketBaseFilteredData } from "@/hooks/usePocektBaseData";
-import { useState } from "react";
-import { Helmet } from "react-helmet-async";
-import LazyImage from "@/utils/LazyImage";
+import Categories from "@/components/Categories"
+import JijoSpinner from "@/components/JijoSpinner"
+import MenuBubble from "@/components/MenuBubble"
+import MenuTitle from "@/components/MenuTitle"
+import Products from "@/components/Products"
+import { usePocketBaseFilteredData } from "@/hooks/usePocektBaseData"
+import { useState } from "react"
+import LazyImage from "@/utils/LazyImage"
+import MenuSearchForm from "@/components/Menu/MenuSearchForm"
+import JiJoHelmet from "@/utils/JiJoHelmet"
+import drinkImage01 from "@/assets/images/menu/drink/drink_image01.jpg"
 
-const collection = 'beverage';
+const collection = "beverage"
 
 function Drink() {
-  const [category, setCategory] = useState('전체보기');//커피, 티, 에이드&주스, 스무디&프라페, 디카페인, 음료, 신상품
-  const {data, status} = usePocketBaseFilteredData(collection, 1, 20, category !== '전체보기' ? `(category~'${category}')` : '');
-  
+  const [category, setCategory] = useState("전체보기") //커피, 티, 에이드&주스, 스무디&프라페, 디카페인, 음료, 신상품
+  const { data, status } = usePocketBaseFilteredData(collection, 1, 20, category !== "전체보기" ? `(category~'${category}')` : "")
 
   const handleCategory = (newCategory) => {
     setCategory(newCategory)
@@ -25,10 +26,10 @@ function Drink() {
 
   return (
     <div>
-      <Helmet>
-        <title>메뉴소개 - 음료</title>
-      </Helmet>
-      <MenuTitle title="MEGA MENU" mainMenu="메뉴소개" subMenu="음료">DRINK MENU</MenuTitle>
+      <JiJoHelmet pageTitle="메뉴소개 - 음료" />
+      <MenuTitle title="MEGA MENU" mainMenu="메뉴소개" subMenu="음료" linkTo="/menu/drink">
+        DRINK MENU
+      </MenuTitle>
       <MenuBubble>
         <strong>깊고 부드러운 커피 맛의 비밀</strong>
         <br />
@@ -37,13 +38,9 @@ function Drink() {
       <section className="bg-white mx-auto max-w-7xl mobile:w-full flex mobile:flex-col justify-between items-center mobile:items-start gap-[3.125rem] pt-[6.25rem] mobile:px-5">
         <div>
           <p className="text-jj_24 font-light">카페 지조 가을시즌 신메뉴</p>
-          <h2 className="text-jj_60 font-bold break-keep leading-tight my-10">
-            풍요로운 가을이 만든 달콤함
-          </h2>
+          <h2 className="text-jj_60 font-bold break-keep leading-tight my-10">풍요로운 가을이 만든 달콤함</h2>
           <div className="text">
-            <p className="title text-jj_22 border-b pb-5">
-              청송 사과 한 잔, 보름달 한 상
-            </p>
+            <p className="title text-jj_22 border-b pb-5">청송 사과 한 잔, 보름달 한 상</p>
             <p className="desc text-[#1c1c1b] opacity-70 pt-5">
               가을이 키운 달콤한 청송 사과 신메뉴와
               <br /> 가을을 닮은 풍요로운 보름달 신메뉴 출시!
@@ -52,32 +49,28 @@ function Drink() {
           </div>
         </div>
         <figure className="shrink-0 tablet:shrink mobile:w-full">
-          <LazyImage
-            src="/src/assets/images/menu/drink/drink_image01.jpg"
-            alt="가을이 키운 달콤한 청송사과 한 잔"
-          />
+          <LazyImage src={drinkImage01} alt="가을이 키운 달콤한 청송사과 한 잔" />
         </figure>
       </section>
       <section className="bg-white mx-auto max-w-7xl mt-[6.25rem] mobile:px-5">
         <div className="titleArea text-center">
           <p className="text-jj_24 font-light">카페 지조의 엄선된 메뉴</p>
           <h2 className="text-jj_60 font-black">JIJO MENU</h2>
-          <span className="text-jj_14 font-light text-[#1c1c1b] opacity-70">
-            ※메뉴 이미지는 연출컷이라 실물과 다를 수 있습니다.
-          </span>
+          <span className="text-jj_14 font-light text-[#1c1c1b] opacity-70">※메뉴 이미지는 연출컷이라 실물과 다를 수 있습니다.</span>
         </div>
 
         <div className="checkboxArea border border-gray-200 p-[1.875rem] my-10">
-          <p className="title text-jj_22 leading-tight pb-5 mb-5 border-b border-b-gray-200">
-            분류보기
-          </p>
-          <Categories name={name} category={category} handleCategory={handleCategory}/>
+          <div className="pb-5 mb-5 border-b border-b-gray-200 flex justify-between items-center">
+            <p className="title text-jj_22 leading-tight">분류보기</p>
+            <MenuSearchForm />
+          </div>
+          <Categories collection="beverage" category={category} handleCategory={handleCategory} />
         </div>
 
-        <Products data={data}/>
+        <Products data={data} />
       </section>
     </div>
-  );
+  )
 }
 
-export default Drink;
+export default Drink
