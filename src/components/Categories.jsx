466bc717f20d23ff510bdd1e6d572debc501bf2a
seonyeongyoon <ssunnyy0204@gmail.com>
2023-09-24 20:46:46 +0900

    📝 readme.MD 작성

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,41 +2,7 @@ import CheckBox from "./CheckBox/CheckBox";
 import {usePocektBaseDataList} from "@/hooks/usePocektBaseData";
 
 function Categories({collection, category, handleCategory}) {
-  // const [categories, setCategories] = useState([
-  //   {
-  //     name: 'all',
-  //     text: '전체보기',
-  //   },
-  //   {
-  //     name: 'coffee',
-  //     text: '커피',
-  //   },
-  //   {
-  //     name: 'ade&juice',
-  //     text: '에이드&주스',
-  //   },
-  //   {
-  //     name: 'smoothe&prape',
-  //     text: '스무디&프라페',
-  //   },
-  //   {
-  //     name: 'decaffein',
-  //     text: '디카페인',
-  //   },
-  //   {
-  //     name: 'beverage',
-  //     text: '음료',
-  //   },
-  //   {
-  //     name: 'new',
-  //     text: '신상품',
-  //   },
-  // ]);
-
-  //const collection = 'beverage';
-  // const {data, status} = usePocektBaseDataList('foods', (fields: 'category'));
-  // console.log(data);
-
+  
   const {data, status} = usePocektBaseDataList(collection, "category");
   const categoryArr = data
     .reduce((acc, item) => {
@@ -53,7 +19,7 @@ function Categories({collection, category, handleCategory}) {
       {categories.map((categoryName) => (
         <CheckBox
           key={categoryName}
-          className="mobile:basis-1/4"
+          className="mobile:basis-[30%]"
           text={categoryName}
           categoryname={categoryName}
           //checked={c.text.replace('&','') === category}
