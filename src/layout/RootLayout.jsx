a38f195f40a5fa4f1553c0cabd685090b434011a
rerro <101159509+rorre0@users.noreply.github.com>
2023-09-24 18:15:52 +0900

    🐛 bug : 메인페이지에서 푸터가 두개 렌더링 되는 이슈 해결

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,32 +1,36 @@
-import { Outlet } from "react-router-dom"
-import Footer from "./Footer/Footer"
-import Header from "./Header/Header"
-import { useEffect } from "react"
-import { getPbImageURL } from "@/utils/getPbImageURL"
-import pb from "@/api/pocketbase"
+import { Outlet, useLocation } from "react-router-dom";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import { useEffect } from "react";
+import { getPbImageURL } from "@/utils/getPbImageURL";
+import pb from "@/api/pocketbase";
 
 //최조1번 image ->imageURL 로 저장
-let mounted = false
+let mounted = false;
 const getFoods = async () => {
-  const foods = await pb.collection("foods").getFullList()
+  const foods = await pb.collection("foods").getFullList();
   for (const food of foods) {
     if (!food.imageURL) {
-      const imageURL = getPbImageURL(food, "image")
+      const imageURL = getPbImageURL(food, "image");
       // console.log(imageURL);
       await pb.collection("foods").update(food.id, {
         imageURL,
-      })
+      });
     }
   }
-}
+};
 
 function RootLayout() {
   useEffect(() => {
     if (!mounted) {
-      getFoods()
-      mounted = true
+      getFoods();
+      mounted = true;
     }
-  }, [])
+  }, []);
+
+  // mainpage 푸터 스타일
+  const location = useLocation();
+  const isMainPage = location.pathname === "/";
 
   return (
     <>
@@ -34,9 +38,9 @@ function RootLayout() {
       <main>
         <Outlet />
       </main>
-      <Footer />
+      {isMainPage ? null : <Footer />}
     </>
-  )
+  );
 }
 
-export default RootLayout
+export default RootLayout;
