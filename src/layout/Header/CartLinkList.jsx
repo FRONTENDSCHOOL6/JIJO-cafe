0608be83d29470a42af4c0491a1d60b7d810da16
Suwan <imissyou5466@gmail.com>
2023-09-25 09:20:23 +0900

    🦁 modify : CartLinkList 장바구니 Count 수정

--- src/layout/Header/CartLinkList.jsx
+++ src/layout/Header/CartLinkList.jsx
@@ -6,14 +6,15 @@ import LazyImage from "@/utils/LazyImage";
 function CartLinkList() {
   /* 인증 정보에 따른 로그인 ➡️ 로그아웃으로 변경 */
   const cart = useCartStore((state) => state.cart);
-  console.log(cart);
+  const cartMapCount = cart.map((item) => item.count);
+  const cartReduce = cartMapCount.reduce((acc, cur) => acc + cur, 0);
 
   return (
     <>
       <LinkList pageLink="/cart" className="relative">
         <LazyImage src={cartBlack} className="w-7 h-7" alt="cart_black_icon" />
         <sup className="cartCount absolute w-5 h-5 -top-4 -right-3 rounded-full  bg-red-500 text-xs text-white flex justify-center items-center p-2">
-          {cart.map((item) => {return Number(item.count)}).reduce((acc, cur) => acc + cur, 0)}
+          {cartReduce ? cartReduce : 0}
         </sup>
       </LinkList>
     </>
